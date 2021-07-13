d31288528d42e8d9d68f98ae12fb555069485fb6
minjikim-gym <kww216@naver.com>
2021-07-13 18:38:45 +0900

    first commit

---
+++ phone_number.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+class phone_number {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        HashMap<String, String> hashMap = new HashMap<String, String>();
+
+        for(int i=0; i<phone_book.length; i++) {
+            hashMap.put(phone_book[i], "prefix");
+        }
+
+        for(String phone : phone_book) {
+            for(int idx=0; idx<phone.length(); idx++) {
+                if(hashMap.containsKey(phone.substring(0, idx))) {
+                    answer = false;
+                }
+            }
+        }
+        return answer;
+    }
+}
---
+++ spy.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class spy {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> clothesMap = new HashMap<String, Integer>();
+
+        for(int i=0; i<clothes.length; i++){
+            clothesMap.put(clothes[i][1], clothesMap.getOrDefault(clothes[i][1], 0)+1);
+        }
+
+        Set<String> keySet = clothesMap.keySet();
+
+        for(String key : keySet) {
+            answer *= clothesMap.get(key)+1;
+        }
+        return answer-1;
+    }
+}
