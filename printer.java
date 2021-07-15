cf4c16f3e2ac54bbc7e328644360260860cf5641
sooarin <kww216@naver.com>
2021-07-15 13:40:09 +0900

    1day 1solve

---
+++ printer.java
@@ -0,0 +1,30 @@
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class printer {
+    public int solution(int[] priorities, int location) {
+        int answer = 1;
+
+        PriorityQueue priority = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int task : priorities){
+            priority.add(task);
+            System.out.println(priority);
+        }
+
+        System.out.println(priority);
+        while(!priority.isEmpty()){
+            for(int i=0; i<priorities.length; i++) {
+                if(priorities[i] == (int)priority.peek()) {
+                    if(i == location){
+                        return answer;
+                    }
+                    priority.poll();
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
